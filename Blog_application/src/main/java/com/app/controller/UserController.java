2d67d457ab27e6d4763049c34ed33b0d70007243
cdac <cdac@rahulprasad2007.noida.cdac.in>
2022-05-18 11:36:14 +0530

    completing user api

--- Blog_application/src/main/java/com/app/controller/UserController.java
+++ Blog_application/src/main/java/com/app/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.dto.UserDto;
+import com.app.payload.ApiResponse;
 import com.app.service.IUserService;
 
 @RestController
@@ -29,13 +35,34 @@ public class UserController {
 	
 	//update user
 	
+	@PutMapping("/")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto , @PathVariable Integer userId){
+		
+	UserDto updatedUser = 	this.userService.updateUser(userDto, userId);
+		return ResponseEntity.ok(updatedUser);
+		
+		
+	}
 	
 	//delte user
 	
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId){
+		this.userService.deleteUser(userId);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("User Deleted successfully" , true),HttpStatus.OK);
+		
+	}
 	
-	//get user
+	//get all user
+	@GetMapping("/")
+	public ResponseEntity<List<UserDto>> getAllUsers(){
+		return ResponseEntity.ok(this.userService.getAllUsers());
+	}
 	
-	
-	
+	//get single user
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> geSingleUsers(@PathVariable Integer userId){
+		return ResponseEntity.ok(this.userService.getUserById(userId));
+	}
 	
 }
